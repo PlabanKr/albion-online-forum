917a2c1afab0843a151896d121707812fdf50d6e
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2023-01-07 19:02:34 +0530

    Add authentication utility functions

---
+++ firebase/authUtils.ts
@@ -0,0 +1,41 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, database } from "./config";
+
+export const registerUserWithEmailAndPassword = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(database, "Users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const loginUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const logout = () => {
+  signOut(auth);
+};
